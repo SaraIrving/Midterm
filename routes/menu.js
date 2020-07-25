9758e4278d65bb1b5fb6819376af7ae6e7164b17
Sara Irving <sarakirving@gmail.com>
2020-07-25 19:03:35 +0000

    add res.render statement to render menu template with menu object as template variables

--- routes/menu.js
+++ routes/menu.js
@@ -13,9 +13,9 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM menu;`)
       .then(data => {
-        const menu = data.rows[0];
+        const menu = data.rows;
         // res.json({ menu }); // returning a promise with the menu as object
-        res.render('index',{ menu })
+        res.render('menu', { menu })
       })
       .catch(err => {
         res
