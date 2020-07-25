b7c16190bd467e30f21b532d37a23c92a1d9687d
Sara Irving <sarakirving@gmail.com>
2020-07-25 02:45:41 +0000

    corrected syntax errors from using a single quote as an apostrophe

--- db/seeds/01_menu_items.sql
+++ db/seeds/01_menu_items.sql
@@ -5,7 +5,7 @@ INSERT INTO menu (item_name, price, description, image_url)
 VALUES ('Chicken Drums', 6.99*100, 'Chicken legs that you can drumb with! you get two', 'https://images.media-allrecipes.com/userphotos/6758784.jpg');
 
 INSERT INTO menu (item_name, price, description, image_url)
-VALUES ('Chicken Wings', 8.99*100, 'How they couldn\'t fly, you get ten', 'https://lh3.googleusercontent.com/-BdoyReqAqqo/UsugFSJiILI/AAAAAAAEW-U/T3GA_qZo-WU/s800/salt-and-pepper-wings-23.jpg');
+VALUES ('Chicken Wings', 8.99*100, 'How they couldn''t fly, you get ten', 'https://lh3.googleusercontent.com/-BdoyReqAqqo/UsugFSJiILI/AAAAAAAEW-U/T3GA_qZo-WU/s800/salt-and-pepper-wings-23.jpg');
 
 INSERT INTO menu (item_name, price, description, image_url)
 VALUES ('Chicken Sandwich', 5.67*100, 'A sandwich of chicken and stuff, you get one', 'https://www.vmcdn.ca/f/files/via/import/2019/01/21100258_dl-chicken-shack-lemon-pepper.jpg;w=960');
@@ -14,14 +14,13 @@ INSERT INTO menu (item_name, price, description, image_url)
 VALUES ('Popcorn Chicken', 7.88*100, 'Pop the chicken into your mouth times fifteen', 'https://thecozyapron.com/wp-content/uploads/2014/09/garlic-popcorn-chicken_09-21-14_1_ca.jpg');
 
 INSERT INTO menu (item_name, price, description, image_url)
-VALUES ('Chicken and Waffles', 6.50*100, 'For breakfast you can eat chicken and waffles', 'https://www.wellseasonedstudio.com/wp-content/uploads/2019/03/Chicken-and-waffles-18.jpg
-');
+VALUES ('Chicken and Waffles', 6.50*100, 'For breakfast you can eat chicken and waffles', 'https://www.wellseasonedstudio.com/wp-content/uploads/2019/03/Chicken-and-waffles-18.jpg');
 
 INSERT INTO menu (item_name, price, description, image_url)
-VALUES ('Coleslaw', 2.89*100, 'I don\'t even know..', 'https://natashaskitchen.com/wp-content/uploads/2019/06/Classic-coleslaw-recipe-3.jpg');
+VALUES ('Coleslaw', 2.89*100, 'I don''t even know..', 'https://natashaskitchen.com/wp-content/uploads/2019/06/Classic-coleslaw-recipe-3.jpg');
 
 INSERT INTO menu (item_name, price, description, image_url)
-VALUES ('Macaroni Salad', 3.10*100, 'It\'s super healthy', 'https://www.thecookierookie.com/wp-content/uploads/2020/05/macaroni-salad-recipe-8-of-8.jpg');
+VALUES ('Macaroni Salad', 3.10*100, 'It''s super healthy', 'https://www.thecookierookie.com/wp-content/uploads/2020/05/macaroni-salad-recipe-8-of-8.jpg');
 
 INSERT INTO menu (item_name, price, description, image_url)
 VALUES ('Tator Tots', 4.50*100, 'Gimme some of your tots!', 'https://cdn.vox-cdn.com/thumbor/GfmTgbKLcN83EdMArLlr5JeiUYs=/0x0:2500x1875/1200x800/filters:focal(1050x738:1450x1138)/cdn.vox-cdn.com/uploads/chorus_image/image/56393387/tater_tots_closeup.0.jpg');
