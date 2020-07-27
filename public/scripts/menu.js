b0060197ea305053724e356865ccf5453ff5b292
Sara Irving <sarakirving@gmail.com>
2020-07-27 19:05:36 +0000

    refactor, trying to figure out why the .remove() is not functioning as expected

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -19,23 +19,32 @@ $(document).ready(function () {
 
     const buildListItem = function(listData) {
       let listItem =
-      `<li><span>${listData.itemName}</span> x ${listData.itemQty}</li>`
+      `<li><span>${listData.itemName}</span> x <p>${listData.itemQty}</p></li>`
 
       return listItem;
     };
     $('.order-list').append(buildListItem(listData));
-    let orderLi = $('.order-list').find('span');
-    let spanMap = $('.order-list span').toArray();
-    for (let item of spanMap) {
-      console.log('item.text() = ', item.text());
-      if (item.text() === (`<span>${itemName}</span>`)) {
-        console.log('hello?')
-        if ($('order-list span').text() === itemName) {
 
-          $('.order-list').append(buildListItem(listData));
-        }
+    // let orderLi = $('.order-list').find('span');
+    $('.order-list li').each(function(i) {
+      console.log($(this).text());
+      if ($(this).find('span').text() === itemName) {
+        console.log('found it');
+        $(this).find('p').val(itemQty);
       }
-    }
+    })
+    // let spanMap = $('.order-list span').toArray();
+    // $('order-list span').val(itemName).remove();
+    // for (let item of spanMap) {
+    //   console.log('item.text() = ', item.text());
+    //   if (item.text() === (`<span>${itemName}</span>`)) {
+    //     console.log('hello?')
+    //     if ($('order-list span').text() === itemName) {
+
+    //       $('.order-list').append(buildListItem(listData));
+    //     }
+    //   }
+    // }
 
 
 
