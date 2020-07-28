f3bcd78df9441715d01e1b37e2be72e6e2ee7fae
Jeremy Huot <logflume@live.com>
2020-07-28 17:00:06 +0000

    Addfunctionality to clear qty inputs and reng order items when user clicks on garbage can icon

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   const buildListItem = function(listData) {
     let classes2 = listData.itemName.replace(/\s+/g, '-').toLowerCase();
     let listItem =
-    `<li class="item-${classes2}"><span>${listData.itemName}</span> x <p class="order-item-qty">${listData.itemQty}</p><h6 class="order-item-price">${listData.itemPrice}</h6></li>`
+    `<li class="item-${classes2}"><span>${listData.itemName}</span> x <p class="order-item-qty">${listData.itemQty}</p><h6 class="order-item-price">${listData.itemPrice}</h6><a href="" class="remove-btn"><img src="/photos/trash.png" width="15" alt="garbage icon"></a></li>`
     return listItem;
   };
 
@@ -11,13 +11,15 @@ $(document).ready(function () {
   $('.item-specifics .item-qty').on('blur', function(event) {
     let itemName = $(this).attr('name');
     let itemQty = $(this).val();
-    let itemPrice = Number($(this).siblings('.item-price').text());
+    let itemPrice = $(this).siblings('.item-price').text(); // used to be wrapped in Number()
     let classes = 'item-' + itemName.replace(/\s+/g, '-').toLowerCase();
     let listData = { itemName, itemQty, itemPrice }
 
     // hide item if below 1 (ideally)
-    if (1 > Number($('.order-list li p').text())) {
-      $('.order-list li').toggleClass('hide');
+    if (0 > Number($('.order-list li p').text())) {
+      $('.order-list li').addClass('hide');
+    } else {
+      $('.order-list li').removeClass('hide');
     }
 
     if($('.order-list').children().length === 0) {
@@ -36,9 +38,33 @@ $(document).ready(function () {
       total += $(this).find('.item-price').text() * $(this).find('.item-qty').val();
     })
 
-    $('.order-total').text(total);
+    $('.order-total').text(parseFloat(total));
 
   })
+
+  $('.order-list').on('click','.remove-btn', function(event) {
+    event.preventDefault();
+    let lineTotal = ($(this).siblings('.order-item-price').text() * $(this).siblings('.order-item-qty').text()).toFixed(2);
+    let currentTotal = Number($('.order-total').text()).toFixed(2);
+    let orderItemName = $(this).siblings('span').text()
+    console.log("current = ", currentTotal);
+    console.log('line = ', lineTotal);
+    console.log("current - line", (currentTotal - lineTotal).toFixed(2));
+
+    $('.order-total').text((currentTotal - lineTotal).toFixed(2));
+
+    $('.item-wrapper').each(function(){
+      if (orderItemName == $(this).find('.item-name').text()) {
+        $(this).find('.item-qty').val('0');
+      }
+    });
+
+    $(this).parent('li').remove();
+
+
+  })
+
+
 
 })
 
