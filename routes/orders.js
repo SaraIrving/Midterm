edaabbe057ccaf5bad9cd3bba3ec144988ab30de
Jeremy Huot <logflume@live.com>
2020-07-28 21:38:48 +0000

    Add functionality to insert orders and items into order_details table, and orders table in SQL db

--- routes/orders.js
+++ routes/orders.js
@@ -25,29 +25,14 @@ module.exports = (db) => {
 
     db.query(`INSERT INTO orders (user_name, user_phone, total, status) VALUES ('${orderName}', ${orderPhone}, ${orderTotal}, 'pending') RETURNING *`)
     .then((data) => {
-
       for (let menuItem of orderItems) {
-        db.query(`INSERT INTO order_details (menu_id, order_id, qty) VALUES ('(SELECT id FROM menu WHERE item_name = '${menuItem[0]}')', ${data.rows.id}, ${menuItem[1]})`);
+          db.query(`INSERT INTO order_details (menu_id, order_id, qty) VALUES ((SELECT id FROM menu WHERE item_name = '${menuItem[0]}'), ${data.rows[0].id}, ${menuItem[1]})`);
       }
       res.redirect('/menu');
     })
     .catch((err) => {
       console.log(err);
     })
-
-    //put order items into order_items table, menu_id/order_id are FK,
-
-    // db.query(`SELECT * FROM menu;`)
-    //   .then(data => {
-    //     const menu = data.rows;
-    //     // res.json({ menu }); // returning a promise with the menu as object
-    //     res.render('menu', { menu })
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
 
   });
   return router;
