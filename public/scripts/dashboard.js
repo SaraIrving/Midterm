15f05f51498f523d529a46efe3cc0c6cce665136
Jeremy Huot <logflume@live.com>
2020-07-30 02:08:06 +0000

    Add visual differences to order items on dashboard

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -5,7 +5,23 @@ $(document).ready(function () {
     if ($(this).find('.order-item-status').text() === 'Completed' || $(this).find('.order-item-status').text() === 'In Progress') {
       $(this).find('.order-form-inputs').css('display','none');
     }
+
+    if ($(this).find('.order-item-status').text() === 'Completed') {
+      $(this).addClass('order-completed');
+    }
+    if ($(this).find('.order-item-status').text() === 'In Progress') {
+      $(this).addClass('order-in-progress');
+    }
+    if ($(this).find('.order-item-status').text() === 'Pending') {
+      $(this).addClass('order-pending');
+    }
+
   })
+
+
+
+
+
 
 });
 
