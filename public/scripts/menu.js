157a9bb46288dd06dfe149f684e7e2fe49ddc105
Jeremy Huot <logflume@live.com>
2020-07-31 17:21:43 +0000

    added comments to menu.js script and then updated variables in query's in new-men-items to prevent bugs coming from users including commas or apostrophes in their input on the form

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,81 +1,77 @@
 $(document).ready(function () {
-
+  // Helper function to build order list items
   const buildListItem = function(listData) {
     let classes2 = listData.itemName.replace(/\s+/g, '-').toLowerCase();
     let listItem =
     `<li class="item-${classes2}"><span>${listData.itemName}</span> x <p class="order-item-qty">${listData.itemQty}</p><h6 class="order-item-price">${listData.itemPrice}</h6><a href="" class="remove-btn"><img src="/photos/trash.png" width="15" alt="garbage icon"></a></li>`
     return listItem;
   };
-
-
-  $('.item-specifics .item-qty').on('blur', function(event) {
+  // Handle on qty inputs which will update order with new / updated items when blurring input
+  $('.item-specifics .item-qty').on('blur', function(e) {
+    // Assign variables to item information
     let itemName = $(this).attr('name');
     let itemQty = $(this).val();
-    let itemPrice = $(this).siblings('.item-price').text(); // used to be wrapped in Number()
+    let itemPrice = $(this).siblings('.item-price').text();
     let classes = 'item-' + itemName.replace(/\s+/g, '-').toLowerCase();
-    let listData = { itemName, itemQty, itemPrice }
+    let listData = { itemName, itemQty, itemPrice } // Passed into buildListItem function to inject information
 
-    // hide item if below 1 (ideally)
+    // Hide item if qty is below 1
     if (0 > Number($('.order-list li p').text())) {
       $('.order-list li').addClass('hide');
     } else {
       $('.order-list li').removeClass('hide');
     }
-
+    // Checks if menu item is already in the order list, if it isn't it adds it, otherwise it will update the qty
     if($('.order-list').children().length === 0) {
+       // There are no items in the list, so add the item
       $('.order-list').append(buildListItem(listData));
-
     } else if($('.order-list li').hasClass(classes)) {
-      //conditional statement to add or delete to total
+      // Item is in the list already, so update it's quantity
       $(`.${classes}`).find('p').text(itemQty);
     } else {
+      // This item isn't in list(but other items are in the list), so add it
       $('.order-list').append(buildListItem(listData));
     }
-
+    // Define total as 0 base
     let total = 0;
+    // Loop through items to find the qty and price of each item, to calculate total for each line item
     $('.item-wrapper').each(function(){
-
+      // Sum each line item to get final total
       total += $(this).find('.item-price').text() * $(this).find('.item-qty').val();
     })
-
+    // Display the updated total
     $('.order-total').text(parseFloat(total));
-
   })
-
-  $('.order-list').on('click','.remove-btn', function(event) {
-    event.preventDefault();
+  // When user clicks garbage icon, remove the item from the order list
+  $('.order-list').on('click','.remove-btn', function(e) {
+    e.preventDefault();
     let lineTotal = ($(this).siblings('.order-item-price').text() * $(this).siblings('.order-item-qty').text()).toFixed(2);
     let currentTotal = Number($('.order-total').text()).toFixed(2);
     let orderItemName = $(this).siblings('span').text()
-    console.log("current = ", currentTotal);
-    console.log('line = ', lineTotal);
-    console.log("current - line", (currentTotal - lineTotal).toFixed(2));
-
+    // Update total to reflect deleted order item
     $('.order-total').text((currentTotal - lineTotal).toFixed(2));
-
+    // Once item is deleted, we reset the menu item input value to 0
     $('.item-wrapper').each(function(){
       if (orderItemName == $(this).find('.item-name').text()) {
         $(this).find('.item-qty').val('0');
       }
     });
-
+    // Remove the line item from order list
     $(this).parent('li').remove();
-
-
   })
-
-  $('.submit-order').on('mousedown',function(event){
+  // When user clicks Place Order register total and display pending banner
+  $('.submit-order').on('mousedown',function(e){
+    // Assign total to input value to send through form
     $('input.order-total').val($('.order-total-span').text());
+     // Display pending banner
     $('body').prepend('<div id="order-pending-banner">Please stay on the page while your order is being confirmed.. <img src="/photos/load.gif" class="loading-gif" alt="loading animation"/></div>');
   });
-
-  $('.submit-order').on('mouseup',function(event){
+  // After user clicks Place Order, change order form to thank you message
+  $('.submit-order').on('mouseup',function(e){
     $('.order-summary').append('<div class="order-done"><h3>Thanks!</h3><p>Your order has been placed.</p></div>');
   });
-
-  $('.order-toggle').on('click',function(event) {dy =
+  // On mobile, our toggle button toggles the form info for optimal user experience
+  $('.order-toggle').on('click',function(e) {
     $('section.menu-wrapper form > div:last-of-type').toggle();
   });
-
-
 })
--- routes/new-menu-item.js
+++ routes/new-menu-item.js
@@ -15,26 +15,26 @@ module.exports = (db) => {
     let newItemName = req.body['new_menu_item']; // either undefined or defined
     let updatedName = req.body['update_item_name'];
     let updatedDescription = req.body['update_item_description'];
-    let updatedPrice = req.body['update_item_price'];
+    let updatedPrice = req.body['update_item_price'] * 100;
     let updatedImage = req.body['update_image_url'];
     let oldName = req.body['menu_item'];
     // Check if we're supposed to add a new item into the menu table in DB
     if (newItemName) {
       // Insert new item into menu table in DB
       db.query(`INSERT INTO menu (item_name, price, description, image_url)
-      VALUES ('${updatedName}', ${updatedPrice}, '${updatedDescription}','${updatedImage}') RETURNING *;`)
+      VALUES ($1, $2, $3, $4) RETURNING *;`, [updatedName, updatedPrice, updatedDescription, updatedImage])
       .then((data) => {
         // Redirect to the edit-menu ejs which will load and display all menu item's including the new item
         res.redirect('/edit-menu');
       });
     } else { // Instead, we're updating an existing menu item in the DB
       db.query(`UPDATE menu
-      SET item_name = COALESCE('${updatedName}', item_name),
-      price = COALESCE(${updatedPrice}, price),
-      description = COALESCE('${updatedDescription}', description),
-      image_url = COALESCE('${updatedImage}', image_url)
-      WHERE item_name = '${oldName}'
-      RETURNING *;`)
+      SET item_name = COALESCE($1, item_name),
+      price = COALESCE($2, price),
+      description = COALESCE($3, description),
+      image_url = COALESCE($4, image_url)
+      WHERE item_name = $5
+      RETURNING *;`, [updatedName, updatedPrice, updatedDescription, updatedImage, oldName])
       .then((data) => {
         // Redirect to the edit-menu ejs which will load and display all menu item's including the updated item
         res.redirect('/edit-menu');
