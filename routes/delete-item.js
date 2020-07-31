433b050d4fdc0cbdd5ac5c596fd9c3a963dc1ab8
Jeremy Huot <logflume@live.com>
2020-07-31 15:56:07 +0000

    add comments and remove testing console.log statement

--- routes/delete-item.js
+++ routes/delete-item.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  // POST to remove an item from the edit-menu admin page
   router.post("/", (req, res) => {
+    // Assign item in question to itemToDelete
     const itemToDelete = req.body['item_name'];
-    console.log("delete router req.body = ", req.body);
+    // Delete itemToDelete from the menu table
     db.query(`DELETE FROM menu WHERE item_name = '${itemToDelete}';`)
-      .then(data => {
+      .then(() => {
+        // Redirect back to edit-menu page which will load all the items except the recently deleted one
         res.redirect('edit-menu');
       })
       .catch(err => {
@@ -15,6 +18,5 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-
   return router;
 };
