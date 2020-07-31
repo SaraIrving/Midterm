fc3f2750d2e0f62cacc91f2ab3f462e249cf949a
Jeremy Huot <logflume@live.com>
2020-07-31 16:14:20 +0000

    add comments and remove testing console.log statements

--- routes/order-update.js
+++ routes/order-update.js
@@ -7,31 +7,32 @@
 
 const express = require('express');
 const router  = express.Router();
-
+// Setup twilio with credentials
 const accountSid = process.env.DB_ACCOUNTSID;
 const authToken = process.env.DB_AUTHTOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 module.exports = (db) => {
+  // Handle the post request from the admin user to update a recently placed order status in the DB
   router.post("/", (req, res) => {
+    // Assign form data to variables
     let orderStatus = req.body['status'];
     let orderId = req.body['order_id'];
     let customerPhone = `+1${req.body['customer_phone']}`;
-    console.log("customerPhone = ", customerPhone);
-
+    // Update order status on orders table in the DB
     db.query(`UPDATE orders SET status = '${orderStatus}' WHERE id = ${orderId} RETURNING *`)
       .then(() => {
+        // Query to get all the order information to send info to customer
         return db.query(`SELECT * FROM orders JOIN order_details ON orders.id = order_id JOIN menu ON menu_id = menu.id WHERE orders.id = ${orderId}`)
-        // res.json({ menu }); // returning a promise with the menu as object
       })
       .then((data) => {
-
+        // Text the customer confirming their order and specifying the wait time
         client.messages.create({
           to: customerPhone,
           from: '+15879068570',
           body: `Your order is confirmed, pickup from Captain Clucks in ${orderStatus} minutes!`
         });
-
+        // Redirect to the dashboard where admin user can see the order status change from pending to in progress
         res.redirect('dashboard');
       })
       .catch(err => {
@@ -40,6 +41,5 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-
   return router;
 };
