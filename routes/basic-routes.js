54c19a8c0f5725f252c6933a2304db5a7fad9961
Jeremy Huot <logflume@live.com>
2020-07-31 15:36:59 +0000

    fix comment syntax

--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -1,22 +1,20 @@
-
-
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-    // render the home page
+    // Render the home page
     router.get("/", (req, res) => {
       res.render("home");
     });
-    // render the login page
+    // Render the login page
     router.get("/login", (req, res) => {
       res.render("login");
     });
-    // allow admin user to login and redirect to dashboard
+    // Allow admin user to login and redirect to dashboard
     router.post("/login", (req,res) => {
       res.redirect('/dashboard')
     });
-    //render the confirmation page for reference purposes
+    // Render the confirmation page for reference purposes
     router.get("/confirm", (req, res) => {
       let confirmedOrder = {};
       res.render("order-confirmation", { confirmedOrder });
